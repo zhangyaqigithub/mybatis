3fcc0f0682cf95c644fd1df70c04b892912e56f5
zhangyaqi <992321066@qq.com>
2021-01-15 17:02:41 +0800

    提交

--- orm-mybatis/src/main/java/com/springboot/mybatis/orm/App.java
+++ orm-mybatis/src/main/java/com/springboot/mybatis/orm/App.java
@@ -14,6 +14,7 @@ import org.springframework.context.ConfigurableApplicationContext;
 @MapperScan("com.springboot.mybatis.orm.dao")
 public class App 
 {
+	
     public static void main( String[] args )
     {
     	ConfigurableApplicationContext context = SpringApplication.run(App.class, args);
